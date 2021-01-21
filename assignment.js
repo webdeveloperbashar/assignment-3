0fd5190a19e680e89e188ab080967685b0b52579
webdeveloperbashar <74394145+webdeveloperbashar@users.noreply.github.com>
2021-01-22 02:28:52 +0600

    assignment full completed

--- assignment.js
+++ assignment.js
@@ -1,29 +1,76 @@
-// Function name list
-// kilometerToMeter()
-//budgetCalculator(watch,mobile,laptop) per price: watch=50,mobile=100,laptop=500
-//hotelCost() per night price: 1-10night=100, 11night-20night=80, 20night=50
-//megaFriend()
-
+//https://github.com/webdeveloperbashar/assignment-3
 
 // Convert Kilometer to Meter
 function kilometerToMeter(kilometer) {
-    var meter;
-    if (kilometer > 0) {
-        meter = kilometer * 1000;
-    }else{
-        meter = 'kilometer cannot be negative'
-    }
-    return meter;
+  var meter;
+  if (kilometer < 0) {
+    meter = "Distance cannot be negative";
+  }else {
+    meter = kilometer * 1000; // 1 kilometer = 1000 meter
+  }
+  return meter;
 }
+var meter = kilometerToMeter(8);
+console.log(meter);
+
 
 // Budget Calculator
-function budgetCalculator(watch,mobile,laptop) {
-    var totalPrice;
-    var watch = watch * 50;
-    var mobile = mobile * 100;
-    var laptop = laptop * 500;
-        totalPrice = watch + mobile + laptop;
-    return totalPrice;
+function budgetCalculator(watch, mobile, laptop) {
+  var totalPrice;
+  if (watch < 0) {
+    totalPrice = "Watch price cannot be negative";
+  }else if(mobile < 0){
+    totalPrice = "Mobile price cannot be negative";
+  }else if(laptop < 0){
+    totalPrice = "Laptop price cannot be negative";
+  }else{
+    var watch = watch * 50; // per watch price 50
+    var mobile = mobile * 100; // per mobile price 100
+    var laptop = laptop * 500; // per laptop price 500
+    totalPrice = watch + mobile + laptop;
+  }
+  return totalPrice;
 }
-var result = budgetCalculator(2,4,8)
+var totalPrice = budgetCalculator(10,30,11);
+console.log(totalPrice);
+
+// Hotel Cost
+function hotelCost(nightCount){
+    var roomRent;
+    if (nightCount < 0) {
+        roomRent = 'Night count cannot be negative';
+    }else if(nightCount <= 10){
+        var firstStep = nightCount * 100; // 1-10 per night = 100
+        roomRent = firstStep;
+    }else if(nightCount <= 20){
+        var getNightCount = nightCount - 10;
+        var secondStep = getNightCount * 80; // 11-20 per night = 80
+        var firstStep = 10 * 100; // 1-10 per night = 100 (Re-assign)
+        roomRent = secondStep + firstStep;
+    }else{
+        var firstStep = 10 * 100; // 1-10 per night = 100 (Re-assign)
+        var secondStep = 10 * 80; // 11-20 per night = 80 (Re-assign)
+        var getNightCount = nightCount - 20;
+        var thirdStep = getNightCount * 50; // 20 more per night = 50
+        roomRent = firstStep + secondStep + thirdStep;
+    }
+    return roomRent;
+}
+var roomRent = hotelCost(21);
+console.log(roomRent);
+
+// Mega Friend
+var names = ['Hasan', 'Mazharul', 'Sakil', 'Mohidul', 'Somrat', 'Riyad']
+function megaFriend(friendNames){
+    var highestName = friendNames[0];
+    for (var i = 0; i < friendNames.length; i++) {
+        var element = friendNames[i];
+        var lengthCheck = element.length; // Name length check
+        if (highestName.length < lengthCheck) {
+            highestName = element // highest value update
+        }
+    }
+    return highestName;
+}
+var result = megaFriend(names)
 console.log(result);
